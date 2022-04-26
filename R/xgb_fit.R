84512f491c78d6cd125aecaff195ed5ae8f4e252
Jean-Olivier Irisson <irisson@normalesup.org>
2022-04-26 21:27:39 +0200

    Better documentation for xgb_fit()

--- R/xgb_fit.R
+++ R/xgb_fit.R
@@ -1,25 +1,38 @@
-#' A generic function to fit an xgboost model
+#' Fit an xgboost model for each row of a `resamples` object
 #'
-#' @param object of class resamples.
-#' @param ... passed to further methods.
+#' @param object of class `resamples` (created by a `resample_***()` function) or
+#'   of class `resamples_grid` (created by `param_grid()`).
+#' @param ... passed to `fit_one_xgb()` and then later to `xgboost::xgb.Train()`
+#'
+#' @inherit fit_one_xgb return
+#'
 #' @export
+#' @examples
+#' # regression
+#' resample_boot(mtcars, 3) %>%
+#'   xgb_fit(resp="mpg", expl=c("cyl", "hp", "qsec"),
+#'     eta=0.1, max_depth=4, nrounds=20)
+#'
+#' resample_boot(mtcars, 2) %>%
+#'   param_grid(eta=c(0.1, 0.2)) %>%
+#'   xgb_fit(resp="mpg", expl=c("cyl", "hp", "qsec"),
+#'     max_depth=4, nrounds=20)
+#'
+#' # classification
+#' mtcarsf <- mutate(mtcars, cyl=factor(cyl))
+#' resample_boot(mtcarsf, 3) %>%
+#'   xgb_fit(resp="cyl", expl=c("mpg", "hp", "qsec"),
+#'     eta=0.1, max_depth=4, nrounds=20)
 xgb_fit <- function(object, ...) {
   UseMethod("xgb_fit")
 }
 
-#' Fit an xgboost model for each row of a `resamples` object
+#' @rdname xgb_fit
 #'
-#' @param object of class resamples, created by a `resample_***()` function.
 #' @param cores integer, number of cores to use for parallel computation.
-#' @param ... passed to `fit_one_xgb()` and then later to `xgboost::xgb.Train()`
 #'
-#' @returns Like `fit_one_xgb()` but with several lines.
-#'
+#' @method xgb_fit resamples
 #' @export
-#' @examples
-#' resample_boot(mtcars, 3) %>%
-#'   xgb_fit(resp="mpg", expl=c("cyl", "hp", "qsec"),
-#'     eta=0.1, max_depth=4, nrounds=20)
 xgb_fit.resamples <- function(object, cores=1, ...) {
   # fit the model for each resample, in parallel
   res <- parallel::mclapply(1:nrow(object), function(i, ...) {
@@ -31,26 +44,10 @@ xgb_fit.resamples <- function(object, cores=1, ...) {
 }
 
 
-#' Fit an xgboost model for each row of a `resamples.grid` object
+#' @rdname xgb_fit
 #'
-#' @param object of class resamples, created by the `param_grid()` function.
-#' @param cores integer, number of cores to use for parallel computation.
-#' @param ... passed to `fit_one_xgb()` and then later to `xgboost::xgb.Train()`
-#'
-#' @returns The input object with an additional column called
-#' `model` containing the fitted model object.
-#'
-#' @returns Like fit_one_xgb() but with several lines and grouped per parameter
-#' combination defined in the grid.
-#'
+#' @method xgb_fit resamples_grid
 #' @export
-#' @examples
-#' m <- resample_boot(mtcars, 2) %>%
-#'   param_grid(eta=c(0.1, 0.5), max_depth=c(2, 4)) %>%
-#'   xgb_fit(resp="mpg", expl=c("cyl", "hp", "qsec"),
-#'           min_child_weight=2, nrounds=20)
-#' m$model[[1]]$params
-#' m$model[[3]]$params
 xgb_fit.resamples_grid <- function(object, cores=1, ...) {
   # extract the names of parameters
   all_names <- names(object)
